cec2accec7f397fdbbd17478979c28ee730f0c1a
jakubrodzen01 <134446932+jakubrodzen01@users.noreply.github.com>
2023-05-24 13:04:01 +0200

    Add files via upload

--- src/main/java/efs.task.oop/Main.java
+++ src/main/java/efs.task.oop/Main.java
@@ -1,7 +1,79 @@
 package efs.task.oop;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        Villager kaysha = new Villager("Kaysha", 30);
+        ExtraordinaryVillager akara = new ExtraordinaryVillager("Akara", 40, ExtraordinaryVillager.Skill.SHELTER);
+        Villager gheed = new Villager("Gheed", 50);
+        ExtraordinaryVillager deckardCain = new ExtraordinaryVillager("Deckard Cain", 85, ExtraordinaryVillager.Skill.IDENTIFY);
+        Villager warriv = new Villager("Warriv", 35);
+        Villager flawia = new Villager("Flawia", 25);
 
+        kaysha.sayHello();
+        akara.sayHello();
+        gheed.sayHello();
+        deckardCain.sayHello();
+        warriv.sayHello();
+        flawia.sayHello();
+
+        Object objectDeckardCain = deckardCain;
+        Object objectAkara = akara;
+
+        //nie da się wykonać metody klasy ExtraordinaryVillager ponieważ są traktowane jako obiekt
+        //objectDeckardCain.sayHello();
+        //objectAkara.sayHello();
+
+        //można wywołać metody klasy ExtraordinaryVillager gdy dokonamy takiego rzutowania
+        //((ExtraordinaryVillager)objectDeckardCain).sayHello();
+        //((ExtraordinaryVillager)objectAkara).sayHello();
+
+        ArrayList<Villager> villagers = new ArrayList<>();
+        villagers.add(kaysha);
+        villagers.add(gheed);
+        villagers.add(warriv);
+        villagers.add(flawia);
+
+        ArrayList<Monster> monsters = new ArrayList<>();
+        monsters.add(Monsters.andariel);
+        monsters.add(Monsters.blacksmith);
+
+        while(Monsters.monstersHealth > 0) {
+            if(villagers.isEmpty()) {
+                System.out.println("Porażka!");
+                break;
+            }
+            if(villagers.get(0).isAlive()==false) {
+                villagers.remove(0);
+                continue;
+            }
+
+            System.out.println("Potwory posiadaja jeszcze " + Monsters.monstersHealth + " punkty zycia");
+            System.out.println("Aktualnie walczacy osadnik to " + villagers.get(0).getName());
+
+            villagers.get(0).attack(monsters.get(0));
+
+            if(monsters.get(0).getHealth() <= 0){
+                monsters.remove(0);
+            }
+
+            if(monsters.isEmpty()){
+                break;
+            }else{
+                monsters.get(0).attack(villagers.get(0));
+            }
+
+        }
+        if(Monsters.monstersHealth <= 0)
+            System.out.println("Obozowisko ocalone!");
+
+        deckardCain = (ExtraordinaryVillager) objectDeckardCain;
+        akara = (ExtraordinaryVillager) objectAkara;
+
+        //można wywołać metody klasy ExtraordinaryVillager
+        //deckardCain.sayHello();
+        //akara.sayHello();
     }
+
 }
---
+++ src/main/java/efs.task.oop/Villager.java
@@ -0,0 +1,50 @@
+package efs.task.oop;
+
+public class Villager implements Fighter {
+    private String name;
+    private int health;
+    private int age;
+
+    public Villager(String name, int age) {
+        this.name = name;
+        this.health = 100;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+    public int getDamage() {
+        double damage = (100 - age * 0.5) / 10;
+        return (int) damage;
+    }
+
+    @Override
+    public void attack(Fighter victim) {
+        int damage = getDamage();
+        victim.takeHit(damage);
+    }
+
+    @Override
+    public void takeHit(int damage) {
+        health -= damage;
+    }
+
+    public boolean isAlive(){
+        if(health > 0)
+            return true;
+        else
+            return false;
+    }
+    public void sayHello() {
+        System.out.println("Greetings traveler... I'm " + name + " and I'm " + age + " years old");
+    }
+}
